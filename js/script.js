7ebaec20a78e530c2988f6d43b076115cdc066eb
leopoldonanti <leonanti21@gmail.com>
2021-04-12 23:31:18 +0200

    ESERCIZIO NON ANKORA ULTIMATO

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-var lista = [ "Tadè", "Logallo", "Rozzi", "Nanti", "Ariton", "Pisanelli", "Gallus" "Paratore", "Acosta" ];
+var lista = ["Tadè", "Logallo", "Rozzi", "Nanti", "Ariton", "Pisanelli", "Gallus", "Paratore", "Acosta"];
 
 var id = prompt("Enter your ID");
 
-lista.push(id)
+lista.push(id);
 
 lista.sort();
 
 console.log(lista);
 
+var z = 0;
+
 for(var i=0; i<lista.length; i++){
   console.log((i+1) + " " + lista[i]);
+  if (lista[z] != id){
+    z++;
+  }
 }
 
-for(var z=0; lista[z] != id; z++){
-}
+/* for(var z=0; lista[z] != id; z++){
+} */
 
 console.log("il cognome inserito è " + id + " ed è in posizione " + (z+1));
